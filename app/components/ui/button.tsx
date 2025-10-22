1a897549fa5988f3b81cc0aa9316a276615e11b8
IreneLee <irene@jgallop.com>
2025-10-22 16:31:05 +0800

    feat(the component of button): add buttonVariants and update style

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        iconOutline: "size-9 rounded-full border border-gray-200 hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        iconGhost: "size-9 rounded-full hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
