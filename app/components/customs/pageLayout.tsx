ef8f759190e560b04feea1cae1caab044b143a67
IreneLee <irene@jgallop.com>
2025-10-22 16:32:22 +0800

    feat(the component of pageLayout): add the method of goBuck and modified style

--- app/components/customs/pageLayout.tsx
+++ app/components/customs/pageLayout.tsx
@@ -1,15 +1,25 @@
 import { cn } from '@/lib/utils';
+import { Button } from '../ui/button';
+import { ArrowLeft } from 'lucide-react';
 
 interface PageLayoutProps {
   title: string;
   children: React.ReactNode;
   className?: string;
+  goBack?: () => void;
 }
 
-export function PageLayout({ title, children, className }: PageLayoutProps) {
+export function PageLayout({ title, children, className, goBack }: PageLayoutProps) {
   return (
     <div className={cn('container flex-1 flex flex-col gap-4', className)}>
-      <h1 className="text-4xl border-b border-gray-200 pb-4">{title}</h1>
+      <div className="flex items-center justify-between border-b border-gray-200 pb-4">
+        <h1 className={cn("text-4xl", !goBack && "w-full")}>{title}</h1>
+        {goBack && (
+          <Button variant="iconOutline" size="icon" onClick={goBack}>
+            <ArrowLeft />
+          </Button>
+        )}
+      </div>
       {children}
     </div>
   );
