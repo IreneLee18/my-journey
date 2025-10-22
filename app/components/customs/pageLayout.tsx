76d3839ea342ee67d797b74e0ebf21acd940547c
IreneLee <irene@jgallop.com>
2025-10-22 16:54:38 +0800

    style(the components of pageLayout): modified the props and style of pageLayout

--- app/components/customs/pageLayout.tsx
+++ app/components/customs/pageLayout.tsx
@@ -1,22 +1,37 @@
 import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
-import { ArrowLeft } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 
 interface PageLayoutProps {
   title: string;
   children: React.ReactNode;
   className?: string;
+  goBackString?: string;
   goBack?: () => void;
 }
 
-export function PageLayout({ title, children, className, goBack }: PageLayoutProps) {
+export function PageLayout({
+  title,
+  children,
+  className,
+  goBackString,
+  goBack,
+}: PageLayoutProps) {
   return (
     <div className={cn('container flex-1 flex flex-col gap-4', className)}>
       <div className="flex items-center justify-between border-b border-gray-200 pb-4">
-        <h1 className={cn("text-4xl", !goBack && "w-full")}>{title}</h1>
-        {goBack && (
-          <Button variant="iconOutline" size="icon" onClick={goBack}>
-            <ArrowLeft />
+        <h1
+          className={cn(
+            'text-4xl font-bold',
+            !goBack && !goBackString && 'w-full'
+          )}
+        >
+          {title}
+        </h1>
+        {goBack && goBackString && (
+          <Button variant="outline" className="flex items-center gap-2" onClick={goBack}>
+            <ChevronLeft />
+            {goBackString}
           </Button>
         )}
       </div>
