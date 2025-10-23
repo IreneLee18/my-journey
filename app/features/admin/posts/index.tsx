5daf9c1da52751ff761d38acba8df95263ac2511
IreneLee <irene@jgallop.com>
2025-10-24 03:53:00 +0800

    feat(feature/admin/posts): add the method of openStatusDialog when user clicks delete

--- app/features/admin/posts/index.tsx
+++ app/features/admin/posts/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
 import { DataTable } from '@/components/customs/dataTable';
 import { PageLayout } from '@/components/customs/pageLayout';
-import { POST_COLUMNS } from './table.config';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { getColumns } from './table.config';
+import { buttonVariants } from '@/components/ui/button';
 import { Link } from 'react-router';
 import { adminPaths } from '@/constants/paths';
 import { cn } from '@/lib/utils';
 import { Plus } from 'lucide-react';
+import { useStatusDialogState } from '@/utils/statusDialogState';
 
 export default function AdminPosts() {
+  const { openStatusDialog } = useStatusDialogState();
+  const onDelete = (id: string) => {
+    openStatusDialog({
+      title: 'Delete Post',
+      description: 'Are you sure you want to delete this post?',
+      status: 'delete',
+      confirmText: 'Delete',
+      cancelText: 'Cancel',
+      onConfirm: () => {
+        console.log('delete', id);
+      },
+    });
+  };
+
   return (
     <PageLayout
       title="Posts"
@@ -39,7 +53,7 @@ export default function AdminPosts() {
               'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800&q=80',
           },
         ]}
-        columns={POST_COLUMNS}
+        columns={getColumns({ onDelete })}
       />
     </PageLayout>
   );
--- app/features/admin/posts/table.config.tsx
+++ app/features/admin/posts/table.config.tsx
@@ -13,52 +13,69 @@ type PostData = {
   image: string;
 };
 
-export const POST_COLUMNS: ColumnDef<PostData>[] = [
-  {
-    size: 200,
-    accessorKey: 'title',
-    header: 'Title',
-  },
-  {
-    size: 300,
-    accessorKey: 'image',
-    header: 'Image',
-    cell: (row) => {
-      const { image, title } = row.row.original;
-      return <img src={image} alt={title} width={100} height={100} />;
+type ColumnProps = {
+  onDelete: (id: string) => void;
+};
+
+export const getColumns = ({
+  onDelete,
+}: ColumnProps): ColumnDef<PostData>[] => {
+  return [
+    {
+      accessorKey: 'title',
+      header: 'Title',
+      cell: (row) => {
+        const { title } = row.row.original;
+        return <span>{title}</span>;
+      },
     },
-  },
-  {
-    accessorKey: 'publishDate',
-    header: 'Publish Date',
-    cell: (row) => {
-      const { publishDate } = row.row.original;
-      return <span>{formatDate(publishDate, 'yyyy/MM/dd')}</span>;
+    {
+      accessorKey: 'image',
+      header: 'Image',
+      cell: (row) => {
+        const { image, title } = row.row.original;
+        return <img src={image} alt={title} width={100} height={100} />;
+      },
     },
-  },
-  {
-    accessorKey: 'action',
-    size: 100,
-    meta: {
-      className: 'text-right',
+    {
+      accessorKey: 'publishDate',
+      header: 'Publish Date',
+      cell: (row) => {
+        const { publishDate } = row.row.original;
+        return <span>{formatDate(publishDate, 'yyyy/MM/dd')}</span>;
+      },
     },
-    header: 'Action',
-    cell: (row) => {
-      return (
-        <div className="flex gap-2">
-          <Link
-            to={adminPaths.postEdit.url}
-            className={cn(buttonVariants({ variant: 'outline',className: 'w-1/2' }))}
-          >
-            <PencilIcon />
-            編輯
-          </Link>
-          <Button variant="outline" className="w-1/2">
-            <TrashIcon />
-            刪除
-          </Button>
-        </div>
-      );
+    {
+      accessorKey: 'action',
+      size: 100,
+      meta: {
+        className: 'text-right',
+      },
+      header: 'Action',
+      cell: (row) => {
+        const { id } = row.row.original;
+        return (
+          <div className="flex gap-2">
+            <Link
+              to={`${adminPaths.postEdit.url}/${id}`}
+              className={cn(
+                buttonVariants({ variant: 'outline', className: 'w-1/2' })
+              )}
+            >
+              <PencilIcon />
+              編輯
+            </Link>
+            <Button
+              variant="outline"
+              className="w-1/2"
+              onClick={() => onDelete(id)}
+            >
+              <TrashIcon />
+              刪除
+            </Button>
+          </div>
+        );
+      },
     },
-  },
-];
+  ];
+};
